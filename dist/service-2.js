59bcf191ecf5e08b7148d5e62e75058e86c46068
charitynguyenvan <charitynguyenvan@gmail.com>
2017-06-09 10:52:24 +0700

    4

--- dist/service-2.js
+++ dist/service-2.js
@@ -72,8 +72,8 @@
 
 "use strict";
 const config = {
-    // host: 'kajkai-redis.xqeh4j.0001.apse1.cache.amazonaws.com',
-    host: 'localhost',
+    host: 'kajkai-redis.xqeh4j.0001.apse1.cache.amazonaws.com',
+    // host: 'localhost',
     port: 6379
 }
 
